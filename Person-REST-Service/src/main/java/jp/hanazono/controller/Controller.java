3d554e37d9e1cc42e47f85133f274dd29553d129
RostreiD <RostreiD@computer-b4d4ca>
2017-03-04 11:49:40 +0200

    Implement geting all Persons

--- Person-REST-Service/src/main/java/jp/hanazono/controller/Controller.java
+++ Person-REST-Service/src/main/java/jp/hanazono/controller/Controller.java
@@ -1,18 +1,27 @@
 package jp.hanazono.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import jp.hanazono.dao.PersonDAO;
 import jp.hanazono.domain.Person;
 
 @RestController("/")
 @RequestMapping("/")
 public class Controller {
+	@Autowired
+	@Qualifier("personDAO")
+	PersonDAO dao;
 
-	@RequestMapping("/")
-	public Person test() {
-		Person person = new Person(0, "fname", "lname", 99);
-		return person;
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public List<Person> getAllPersons() {
+
+		return dao.getAllPersons();
 
 	}
 
