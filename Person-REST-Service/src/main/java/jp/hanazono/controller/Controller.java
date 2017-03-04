1798df70b24af1aed30d25f2113c4d8199e08edb
RostreiD <RostreiD@computer-b4d4ca>
2017-03-04 12:26:37 +0200

    Implement all methods in Controller

--- Person-REST-Service/src/main/java/jp/hanazono/controller/Controller.java
+++ Person-REST-Service/src/main/java/jp/hanazono/controller/Controller.java
@@ -2,6 +2,8 @@ package jp.hanazono.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,36 @@ public class Controller {
 
 		return dao.getAllPersons();
 
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.DELETE)
+	public List<Person> deletePerson(HttpServletRequest request) {
+		Integer id = Integer.parseInt(request.getParameter("id"));
+		dao.deletePerson(dao.findByID(id));
+		return dao.getAllPersons();
+	}
+	
+	@RequestMapping(value="/", method = RequestMethod.PUT)
+	public List<Person> updatePerson(HttpServletRequest request)
+	{
+		Person person = new Person();
+		person.setId(Integer.parseInt(request.getParameter("id")));
+		person.setFirstname(request.getParameter("firstname"));
+		person.setLastname(request.getParameter("lastname"));
+		person.setAge(Integer.parseInt(request.getParameter("age")));
+		dao.updatePerson(person);
+		return dao.getAllPersons();
+	}
+	
+	@RequestMapping(value="/", method = RequestMethod.POST)
+	public List<Person> addPerson(HttpServletRequest request)
+	{
+		Person person = new Person();
+		person.setFirstname(request.getParameter("firstname"));
+		person.setLastname(request.getParameter("lastname"));
+		person.setAge(Integer.parseInt(request.getParameter("age")));
+		dao.addPerson(person);
+		return dao.getAllPersons();
 	}
 
 }
