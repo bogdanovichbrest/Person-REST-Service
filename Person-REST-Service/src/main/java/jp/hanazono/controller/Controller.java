b2070392b797e125a0c3b66451a98f12626e291f
RostreiD <RostreiD@computer-b4d4ca>
2017-03-07 09:00:18 +0200

    some changes

--- Person-REST-Service/src/main/java/jp/hanazono/controller/Controller.java
+++ Person-REST-Service/src/main/java/jp/hanazono/controller/Controller.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,33 +29,20 @@ public class Controller {
 	}
 
 	@RequestMapping(value = "/", method = RequestMethod.DELETE)
-	public List<Person> deletePerson(HttpServletRequest request) {
-		Integer id = Integer.parseInt(request.getParameter("id"));
-		dao.deletePerson(dao.findByID(id));
-		return dao.getAllPersons();
+	public void deletePerson(@RequestBody Person person) {
+		System.out.println(person.getId());
+		dao.deletePerson(person);
 	}
-	
-	@RequestMapping(value="/", method = RequestMethod.PUT)
-	public List<Person> updatePerson(HttpServletRequest request)
-	{
-		Person person = new Person();
-		person.setId(Integer.parseInt(request.getParameter("id")));
-		person.setFirstname(request.getParameter("firstname"));
-		person.setLastname(request.getParameter("lastname"));
-		person.setAge(Integer.parseInt(request.getParameter("age")));
+
+	@RequestMapping(value = "/", method = RequestMethod.PUT)
+	public void updatePerson(Person person) {
 		dao.updatePerson(person);
-		return dao.getAllPersons();
 	}
-	
-	@RequestMapping(value="/", method = RequestMethod.POST)
-	public List<Person> addPerson(HttpServletRequest request)
-	{
-		Person person = new Person();
-		person.setFirstname(request.getParameter("firstname"));
-		person.setLastname(request.getParameter("lastname"));
-		person.setAge(Integer.parseInt(request.getParameter("age")));
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public void addPerson(@RequestBody Person person) {
+
 		dao.addPerson(person);
-		return dao.getAllPersons();
 	}
 
 }
